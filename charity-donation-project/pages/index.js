05e8fcb2fc389523ca4c8e833a8dbc1db1a969cf
Nuwan Chamikara <39343048+gmnchamikara@users.noreply.github.com>
2022-09-13 10:41:34 +0530

    app created

---
+++ charity-donation-project/pages/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import BasicPageWrapper from "../components/layouts/BasicPageWrapper";
+
+export default function Home() {
+  return <BasicPageWrapper>this is the home</BasicPageWrapper>;
+}
